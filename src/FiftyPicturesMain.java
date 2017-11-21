563b076926323f3f58bfb3c4796c08351fbf8fef
BohdanRadchuk <shpionuse@gmail.com>
2017-11-21 15:17:56 +0200

    25 pictures ready

--- src/FiftyPicturesMain.java
+++ src/FiftyPicturesMain.java
@@ -1,12 +1,88 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class FiftyPicturesMain extends Application {
 
+    public static final String LOAD_FROM = "files/links.txt";
+    private ArrayList<URL> urls = new ArrayList<>(50);
+
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void fileLinksReading() {
+        File file = new File(LOAD_FROM);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextLine()) {
+            try {
+                this.urls.add(new URL(scanner.nextLine()));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void showPictures(Pane root) {
+        fileLinksReading();
+        Random random = new Random();
+        final int processors = Runtime.getRuntime().availableProcessors();
+        ExecutorService pool = Executors.newFixedThreadPool(processors);
+        int rows = 0;
+        for (int i = 0; i < 25; i++) {
+            ImageView imageView = new ImageView();
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(100);
+            if (i > 0 && (i % 5) == 0) {
+                rows++;
+            }
+            if (rows > 0)
+                imageView.setTranslateY(100 * rows);
+            imageView.setTranslateX(100 * (i % 5));
+            root.getChildren().addAll(imageView);
+            pool.submit(() -> {
+                try {
+                    URL url = urls.get(random.nextInt(50));
+                    Image image = new Image(url.openStream());
+                    Platform.runLater(() -> {
+                        imageView.setImage(image);
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        pool.shutdown();
+    }
+
+    public void pressButton(Pane root){
+        Button button = new Button("Обновить");
+        button.setTranslateX(510);
+        button.setOnAction(event -> {
+            root.getChildren().clear();
+            root.getChildren().addAll(button);
+            showPictures(root);});
+        root.getChildren().addAll(button);
     }
 
     @Override
@@ -14,8 +90,10 @@ public class FiftyPicturesMain extends Application {
         Pane root = new Pane();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setWidth(500);
-        primaryStage.setHeight(500);
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(600);
+        primaryStage.show();
+        pressButton(root);
 
     }
 }
--- src/TaskTwoMain.java
+++ src/TaskTwoMain.java
@@ -7,30 +7,30 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class TaskTwoMain {
     static final int ARRAY_SIZE = 80000;
-    static double sumDouble=0;
+    static double sumDouble = 0;
     static AtomicReference<Double> summ = new AtomicReference<>(0.0);
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Semaphore semaphorePoolvsThread = new Semaphore(1);
         ArrayOperations arrayOps = new ArrayOperations();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many repeats do you want to do?");
-        int repeats = scanner.nextInt();
-        arrayOps.intArrayCreation(ARRAY_SIZE);
+        //System.out.println("How many repeats do you want to do?");
+        //int repeats = scanner.nextInt();
+        int repeats = 1;
+                arrayOps.intArrayCreation(ARRAY_SIZE);
         final int processors = Runtime.getRuntime().availableProcessors();
         System.out.println("amount of processors = " + processors);
 
 
         long timer = System.currentTimeMillis();
-        System.out.println("Time at start of pool work = "+ timer);
+        System.out.println("Time at start of pool work = " + timer);
         ExecutorService threadPool = Executors.newFixedThreadPool(processors);
         while (repeats > 0) {
             semaphorePoolvsThread.acquire();
             summ.set(0.0);
             for (int i = 0; i < processors; i++) {
                 int finalI = i;
-                Callable<Double> doubleCallable = () -> {
-                    return arrayOps.calculateResult(finalI * (ARRAY_SIZE / processors), (finalI + 1) * ARRAY_SIZE / processors);
-                };
+                Callable<Double> doubleCallable = () -> arrayOps.calculateResult(finalI * (ARRAY_SIZE / processors), (finalI + 1) * ARRAY_SIZE / processors);
                 FutureTask<Double> futureTask = new FutureTask<>(doubleCallable);
 
                 threadPool.submit(futureTask);
@@ -42,43 +42,46 @@ public class TaskTwoMain {
 
 
             semaphorePoolvsThread.release();
-        repeats --;}
+            repeats--;
+        }
         threadPool.shutdown();
         timer -= System.currentTimeMillis();
-        System.out.println("pool was working for " + (-1)*timer + "ms");
+        System.out.println("pool was working for " + (-1) * timer + "ms");
 
 
         semaphorePoolvsThread.acquire();
 
         summ.set(0.0);
         sumDouble = 0;
-        for (int i = 0; i<processors; i++){
+
+        for (int i = 0; i < processors; i++) {
             final int count = i;
-            double tempFuture=0;
+            double tempFuture = 0;
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                   // Thread.currentThread().wait(1000);
-                     double tempF = arrayOps.calculateResult(count*(ARRAY_SIZE/processors),(count+1)*ARRAY_SIZE/processors);
-                    summ.set(summ.get() + arrayOps.calculateResult(count*(ARRAY_SIZE/processors),(count+1)*ARRAY_SIZE/processors));
-                    System.out.println(sumDouble);
+                    // Thread.currentThread().wait(1000);
+                    System.out.println(arrayOps.calculateResult(count * (ARRAY_SIZE / processors), (count + 1) * ARRAY_SIZE / processors));
+                    arrayOps.calculateResult(count * (ARRAY_SIZE / processors), (count + 1) * ARRAY_SIZE / processors);
+                    /*summ.set(summ.get() + arrayOps.calculateResult(count*(ARRAY_SIZE/processors),(count+1)*ARRAY_SIZE/processors));
+                    System.out.println(sumDouble);*/
                     //sumDouble+=arrayOps.calculateResult(0,80000);
                     /*
                     System.out.println(summ.get() + "atomic");
-                */}
+                */
+                }
             };
 
-            FutureTask <Double> futureTask = new FutureTask<Double>(runnable, tempFuture);
-            Thread thread = new Thread(runnable);
-        System.out.println(thread.getName());
-
-        thread.start();
-        sumDouble +=futureTask.get();
+            FutureTask<Double> futureTask = new FutureTask<Double>(runnable, tempFuture);
+            new Thread(futureTask).start();
+            System.out.println(futureTask.get() + "future get");
+            sumDouble += futureTask.get();
+            System.out.println(sumDouble + "summdouble at end");
 
         }
 
         semaphorePoolvsThread.release();
-        System.out.println("summdouble" + sumDouble);
+        System.out.println("summdouble" + sumDouble );
         System.out.println(summ.get());
     }
 }
