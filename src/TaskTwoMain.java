b44eaf5076e220b331fbbe6825f3f9e9eb088acc
BohdanRadchuk <shpionuse@gmail.com>
2017-11-21 15:57:40 +0200

    task 2 ready

--- src/TaskTwoMain.java
+++ src/TaskTwoMain.java
@@ -7,17 +7,16 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class TaskTwoMain {
     static final int ARRAY_SIZE = 80000;
-    static double sumDouble = 0;
     static AtomicReference<Double> summ = new AtomicReference<>(0.0);
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Semaphore semaphorePoolvsThread = new Semaphore(1);
         ArrayOperations arrayOps = new ArrayOperations();
         Scanner scanner = new Scanner(System.in);
-        //System.out.println("How many repeats do you want to do?");
-        //int repeats = scanner.nextInt();
-        int repeats = 1;
-                arrayOps.intArrayCreation(ARRAY_SIZE);
+        System.out.println("How many repeats do you want to do?");
+        int totalRepeats = scanner.nextInt();
+
+        arrayOps.intArrayCreation(ARRAY_SIZE);
         final int processors = Runtime.getRuntime().availableProcessors();
         System.out.println("amount of processors = " + processors);
 
@@ -25,63 +24,51 @@ public class TaskTwoMain {
         long timer = System.currentTimeMillis();
         System.out.println("Time at start of pool work = " + timer);
         ExecutorService threadPool = Executors.newFixedThreadPool(processors);
+        semaphorePoolvsThread.acquire();
+        int repeats = totalRepeats;
         while (repeats > 0) {
-            semaphorePoolvsThread.acquire();
             summ.set(0.0);
             for (int i = 0; i < processors; i++) {
                 int finalI = i;
-                Callable<Double> doubleCallable = () -> arrayOps.calculateResult(finalI * (ARRAY_SIZE / processors), (finalI + 1) * ARRAY_SIZE / processors);
+                Callable<Double> doubleCallable = () -> arrayOps.calculateResult
+                        (finalI * (ARRAY_SIZE / processors), (finalI + 1) * ARRAY_SIZE / processors);
                 FutureTask<Double> futureTask = new FutureTask<>(doubleCallable);
-
                 threadPool.submit(futureTask);
-                sumDouble += futureTask.get();
                 summ.set(summ.get() + futureTask.get());
 
             }
             System.out.println("Total AtomicSumm = " + summ + " from thread pool");
-
-
-            semaphorePoolvsThread.release();
             repeats--;
         }
-        threadPool.shutdown();
+        semaphorePoolvsThread.release();
         timer -= System.currentTimeMillis();
         System.out.println("pool was working for " + (-1) * timer + "ms");
+        threadPool.shutdown();
 
 
         semaphorePoolvsThread.acquire();
-
-        summ.set(0.0);
-        sumDouble = 0;
-
-        for (int i = 0; i < processors; i++) {
-            final int count = i;
-            double tempFuture = 0;
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    // Thread.currentThread().wait(1000);
-                    System.out.println(arrayOps.calculateResult(count * (ARRAY_SIZE / processors), (count + 1) * ARRAY_SIZE / processors));
-                    arrayOps.calculateResult(count * (ARRAY_SIZE / processors), (count + 1) * ARRAY_SIZE / processors);
-                    /*summ.set(summ.get() + arrayOps.calculateResult(count*(ARRAY_SIZE/processors),(count+1)*ARRAY_SIZE/processors));
-                    System.out.println(sumDouble);*/
-                    //sumDouble+=arrayOps.calculateResult(0,80000);
-                    /*
-                    System.out.println(summ.get() + "atomic");
-                */
-                }
-            };
-
-            FutureTask<Double> futureTask = new FutureTask<Double>(runnable, tempFuture);
-            new Thread(futureTask).start();
-            System.out.println(futureTask.get() + "future get");
-            sumDouble += futureTask.get();
-            System.out.println(sumDouble + "summdouble at end");
-
+        Semaphore threadSemaphore = new Semaphore(processors);
+        long timer1 = System.currentTimeMillis();
+        System.out.println("Time at start of Thread work = " + timer1);
+        repeats = totalRepeats;
+        while (repeats > 0) {
+            summ.set(0.0);
+            threadSemaphore.acquire();
+            for (int i = 0; i < processors; i++) {
+                int finalI = i;
+                Callable<Double> threadCallable = () -> arrayOps.calculateResult
+                        (finalI * (ARRAY_SIZE / processors), (finalI + 1) * ARRAY_SIZE / processors);
+                FutureTask<Double> futureTask = new FutureTask<>(threadCallable);
+                new Thread(futureTask).start();
+                summ.set(summ.get() + futureTask.get());
+            }
+            System.out.println("Total AtomicSumm = " + summ + " from threads");
+            repeats--;
+            threadSemaphore.release();
         }
-
         semaphorePoolvsThread.release();
-        System.out.println("summdouble" + sumDouble );
-        System.out.println(summ.get());
+        timer1 -= System.currentTimeMillis();
+        System.out.println("Threads was working for " + (-1) * timer1 + "ms");
+
     }
 }
